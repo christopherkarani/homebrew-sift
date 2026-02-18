31a5182f4d678779efc46b3a69012c4d4db16d77
christopherkarani <chrisbkarani@gmail.com>
2026-02-18 19:00:36 +0300

    Fix sift formula release tag and checksum for 0.0.1

--- Formula/sift.rb
+++ Formula/sift.rb
@@ -1,8 +1,8 @@
 class Sift < Formula
   desc "Semantic git history CLI powered by Wax"
   homepage "https://github.com/christopherkarani/Sift"
-  url "https://github.com/christopherkarani/Sift/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  url "https://github.com/christopherkarani/Sift/archive/refs/tags/0.0.1.tar.gz"
+  sha256 "ea6d5f85a6fc5045be59cff5408111e3bc819dcf42381926b8eea8a3eda468c0"
   license "Apache-2.0"
 
   depends_on "swift" => :build
