e4b1f02b920c0ee6e70688ba98beed012e1ec26a
christopherkarani <chrisbkarani@gmail.com>
2026-02-18 20:33:39 +0300

    Bump sift formula to 0.0.2 (fix tui crash)

--- Formula/sift.rb
+++ Formula/sift.rb
@@ -1,8 +1,8 @@
 class Sift < Formula
   desc "Semantic git history CLI powered by Wax"
   homepage "https://github.com/christopherkarani/Sift"
-  url "https://github.com/christopherkarani/Sift/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "ea6d5f85a6fc5045be59cff5408111e3bc819dcf42381926b8eea8a3eda468c0"
+  url "https://github.com/christopherkarani/Sift/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "e174e2f0449925f7831e382c4afd7288e9b9cd6d10b2bd67653eaf6e59466553"
   license "Apache-2.0"
 
   depends_on "swift" => :build
